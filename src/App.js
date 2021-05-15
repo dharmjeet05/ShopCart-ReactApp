551d435169a2ef758924ca2cda0cb8f87895c858
Dharmjeet Vaishnav <dharmjeetvaishnav@gmail.com>
2021-05-16 00:27:25 +0530

    all methods are done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,38 @@
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "react-toastify/dist/ReactToastify.css";
+import "./App.css";
+import { toast } from "react-toastify";
+
 const App = () => {
+  const [cartItem, setCartItem] = useState([]);
+
+  const addInCart = (item) => {
+    const isAlreadyAdded = cartItem.findIndex(function (array) {
+      return array.id === item.id;
+    });
+
+    if (isAlreadyAdded !== -1) {
+      toast("Already added in cart", {
+        type: "error",
+      });
+    }
+
+    setCartItem([...cartItem, item]);
+  };
+
+  const buyNow = () => {
+    setCartItem([]);
+
+    toast("Purchase Complate", {
+      type: "success",
+    });
+  };
+
+  const reamoveItem = (item) => {
+    setCartItem(cartItem.filter((singleItem) => singleItem.id !== item.id));
+  };
+
   return (
     <div className="App">
       <h1>Hello</h1>
